a9b6f630de38be5d8222db53d4569043d5f60af8
Gilmar Souza <gilmarj@ciandt.com>
2015-05-10 17:38:20 -0300

    Plot 2

---
+++ plot2.R
@@ -0,0 +1,28 @@
+library(dplyr)
+library(lubridate)
+
+Sys.setlocale(category = "LC_ALL", locale = "en_US.UTF-8")
+setwd('/home/gilmarj/courses/coursera/exdata-014/ExData_Plotting1')
+if(!file.exists("./data")){dir.create("./data")}
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+file <- tempfile()
+download.file(fileUrl,method="curl",file)
+
+df <- read.csv(unz(file,"household_power_consumption.txt"),sep=';')
+
+unlink(file)
+
+names(df) <- tolower(names(df))
+df$date <- as.Date(df$date,format='%d/%m/%Y')
+df$time <- strptime(df$time)
+df$global_active_power <- as.numeric(as.character(df$global_active_power))
+
+hpc <- filter(df, date >= '2007-02-01' & date <= '2007-02-02')
+
+hpc$datetime <- strptime(paste(hpc$date, hpc$time), "%Y-%m-%d %H:%M:%S")
+
+png(filename = './plot2.png', width = 480, height = 480)
+with(hpc, plot(datetime, global_active_power, type="l",col="black",
+               xlab="",ylab='Global Active Power (kilowatts)'))
+dev.off()
